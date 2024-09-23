fea2c8de8f9948510f6a469ade6ba9fbfecdd5e4
Baelak <teklemichaelkaleab@gmail.com>
2024-09-22 23:40:20 -0400

    Baseline code and structuring done

--- Develop/models/user.js
+++ Develop/models/user.js
@@ -1,31 +1,35 @@
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+
+const sequelize = require('../config/connection.js');
 
 class User extends Model {}
 
 User.init(
   {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
+   id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+   },
+   username:{
+    type:DataTypes.STRING,
+    allowNull:false,
+   },
+   email:{
+    type:DataTypes.STRING,
+    allowNull:false,
+   },
+   password:{
+    type:DataTypes.STRING,
+    allowNull:false,
+   },
   },
   {
     sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
     modelName: 'user',
   }
 );
