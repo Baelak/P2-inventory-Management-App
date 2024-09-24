35d347a8276e706270b79d021c004a9738a354d3
Baelak <teklemichaelkaleab@gmail.com>
2024-09-23 21:22:51 -0400

    db url

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,15 +1,19 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost', // Change to Render database host - REFER MODULE 11
-    dialect: 'postgres',
-    port: 5432, // Default PostgreSQL port
-  }
-);
+let sequelize;
 
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: 'localhost',
+      dialect: 'postgres',
+    },
+  );
+}
 module.exports = sequelize;
